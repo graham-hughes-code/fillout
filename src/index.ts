fba7f18d5ffa5420a13c201e06307ca0d45915d2
graham-hughes-code <grahamhughes@optonline.net>
2024-03-07 15:57:46 -0500

    fix-pagination

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,13 @@ const filter_response = (
 app.get(
   "/api/forms/:formId/submissions",
   async (req: Request, res: Response) => {
+    // checking auth exists
     const auth_token = req.header("Authorization");
     if (!auth_token) {
       return res.status(403).json({ message: "auth-token missing" });
     }
+
+    // pulling out needed params
     const { formId } = req.params;
     const query_params: any = req.query;
 
@@ -62,28 +65,54 @@ app.get(
       : undefined;
     delete query_params.filters;
 
-    const limit = query_params.limit ? query_params.limit : 150;
+    const offset: number = query_params.offset ? query_params.offset : 0;
+    delete query_params.offset;
+    const limit: number = query_params.limit ? query_params.limit : 150;
+    delete query_params.limit;
 
-    let response: any = {};
+    // pull data from from Fillout api
+    let response: any = null;
 
     try {
-      let fillout_response = await fetch(
-        `https://api.fillout.com/v1/api/forms/${formId}/submissions` +
-          "?" +
-          new URLSearchParams(query_params).toString(),
-        {
-          headers: { Authorization: auth_token },
+      const page_size = 150;
+      let last_reponse_len = 1;
+      let page_offset = 0;
+
+      while (last_reponse_len != 0) {
+        let fillout_response = await fetch(
+          `https://api.fillout.com/v1/api/forms/${formId}/submissions` +
+            "?" +
+            new URLSearchParams({
+              offset: page_offset,
+              limit: page_size,
+              ...query_params,
+            }).toString(),
+          {
+            headers: { Authorization: auth_token },
+          }
+        );
+        const fillout_response_json = await fillout_response.json();
+        if (!fillout_response.ok) {
+          return res
+            .status(fillout_response.status)
+            .json(fillout_response_json);
         }
-      );
-      const fillout_response_json = await fillout_response.json();
-      if (!fillout_response.ok) {
-        return res.status(fillout_response.status).json(fillout_response_json);
+
+        last_reponse_len = fillout_response_json.responses;
+
+        if (!response) {
+          response = fillout_response_json;
+        } else if (last_reponse_len > 0) {
+          response.responses.push(fillout_response_json.responses);
+        }
+
+        page_offset += page_size;
       }
-      response = fillout_response_json;
     } catch (error) {
       return res.status(500).json({ message: "server error" });
     }
 
+    // filter responses
     if (filters) {
       const api_response_filtered = filter_response(
         response.responses,
@@ -92,11 +121,18 @@ app.get(
       response = {
         ...response,
         responses: api_response_filtered,
-        totalResponses: response.totalResponses - api_response_filtered.length,
-        pageCount:
-          (response.totalResponses - api_response_filtered.length) / limit,
       };
     }
+
+    // add back pagination
+
+    response = {
+      ...response,
+      totalResponses: response.responses.length,
+      pageCount: Math.ceil(response.responses.length / limit),
+    };
+
+    response.responses = response.responses.slice(offset, offset + limit);
 
     return res.send(response);
   }
